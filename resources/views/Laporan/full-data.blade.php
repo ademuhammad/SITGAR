0e2fd0c29f908502c559f93ea537fd515a36b239
Adeveloper <ademnurhadi@gmail.com>
2024-08-06 10:46:11 +0700

    up

--- resources/views/Laporan/full-data.blade.php
+++ resources/views/Laporan/full-data.blade.php
@@ -270,9 +270,9 @@
                             <tfoot>
                                 <tr>
                                     <th colspan="8" style="text-align: right;">Total:</th>
-                                    <th id="total-nilai-rekomendasi">{{ number_format($totalNilaiRekomendasi ?? 0, 2) }}</th>
-                                    <th id="total-nilai-telah-dibayar">{{ number_format($totalNilaiTelahDibayar ?? 0, 2) }}</th>
-                                    <th id="total-sisa-nilai-uang">{{ number_format($totalSisaNilaiUang ?? 0, 2) }}</th>
+                                    <th id="total-nilai-rekomendasi">Rp.{{ number_format($totalNilaiRekomendasi ?? 0, 2, ',', '.') }}</th>
+                                    <th id="total-nilai-telah-dibayar">Rp.{{ number_format($totalNilaiTelahDibayar ?? 0, 2, ',', '.') }}</th>
+                                    <th id="total-sisa-nilai-uang">Rp.{{ number_format($totalSisaNilaiUang ?? 0, 2, ',', '.') }}</th>
                                 </tr>
 
                             </tfoot>
